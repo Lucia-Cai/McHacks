78955ad0aebaafeb81bf73ecc77f54b06c485a6d
Marianne Romero <marianne.romero@outlook.com>
2024-01-27 15:08:25 -0500

    can add multiple tables now

--- flashcard_app_database/flashcards_database.py
+++ flashcard_app_database/flashcards_database.py
@@ -2,54 +2,58 @@
 
 import sqlite3
 
-table_name = 'flashcards'
+# table_name = 'flashcards'
 
-CREATE_FLASHCARDS_TABLE = f'CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, question TEXT, answer TEXT, result BOOLEAN);'
+# CREATE_FLASHCARDS_TABLE = f'CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, question TEXT, answer TEXT, result BOOLEAN);'
 
-ADD_FLASHCARD = f'INSERT INTO {table_name} (question, answer, result) VALUES (?, ?, ?);'
+# ADD_FLASHCARD = f'INSERT INTO {table_name} (question, answer, result) VALUES (?, ?, ?);'
 
-GET_ALL_FLASHCARDS = f'SELECT * FROM {table_name};'
+# GET_ALL_FLASHCARDS = f'SELECT * FROM {table_name};'
 
-GET_WRONG_FLASHCARDS = f'SELECT * FROM {table_name} WHERE relsult = False;'
+# GET_WRONG_FLASHCARDS = f'SELECT * FROM {table_name} WHERE relsult = False;'
 
-CHANGE_RESULT = f'''
-UPDATE {table_name}
-SET result = (?)
-WHERE question = (?)'''
+# CHANGE_RESULT = f'''
+# UPDATE {table_name}
+# SET result = (?)
+# WHERE question = (?)'''
 
-DELETE_ROW = f'DELETE FROM {table_name} WHERE question = (?)'
+# DELETE_ROW = f'DELETE FROM {table_name} WHERE question = (?)'
 
 def connect():
     return sqlite3.connect('flashcards_database.db') # sqlite3 works with files, if file does not exist it creates it
 
-def create_tables(connection):
+def create_tables(connection, table_name):
     with connection:
-        connection.execute(CREATE_FLASHCARDS_TABLE)
+        connection.execute(f'CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, question TEXT, answer TEXT, result BOOLEAN);')
 
-def add_flashcard(connection, question, answer, result = False):
+def add_flashcard(connection, table_name, question, answer, result = False):
     with connection:
-        connection.execute(ADD_FLASHCARD, (question, answer, result))
+        connection.execute(f'INSERT INTO {table_name} (question, answer, result) VALUES (?, ?, ?);', (question, answer, result))
 
 def dict_to_flashcards(connection, dict):
     for question, answer in enumerate(dict.items()):
         add_flashcard(connection, question, answer)
 
-def get_all_flashcards(connection):
+def get_all_flashcards(connection, table_name):
     with connection:
-        return connection.execute(GET_ALL_FLASHCARDS).fetchall()
+        return connection.execute(f'SELECT * FROM {table_name};').fetchall()
 
-def get_wrong_flashcards(connection):
+def get_wrong_flashcards(connection, table_name):
     with connection:
-        return connection.execute(GET_WRONG_FLASHCARDS).fetchall()
+        return connection.execute(f'SELECT * FROM {table_name} WHERE relsult = False;').fetchall()
 
-def update_answer(connection, question, new_result):
+def update_answer(connection, table_name, question, new_result):
     with connection:
-        connection.execute(CHANGE_RESULT, (table_name, new_result, question))
+        connection.execute(f'''
+                            UPDATE {table_name}
+                            SET result = (?)
+                            WHERE question = (?)'''\
+                                , (table_name, new_result, question))
 
 
-def remove_question(connection, question):
+def remove_question(connection, table_name, question):
     with connection:
-        connection.execute(DELETE_ROW, (question))
+        connection.execute(f'DELETE FROM {table_name} WHERE question = (?)', (question))
         connection.commit()
 
 
--- flashcard_app_database/menu.py
+++ flashcard_app_database/menu.py
@@ -5,25 +5,25 @@ from flashcards_database import *
 
 # if you want to create flashcard use: add_flashcard(connection, 'question', 'answer')
 
-def ask_wrong_flashcards(connection, wrong_flashcards):
+def ask_wrong_flashcards(connection, table_name, wrong_flashcards):
     for question in all_flashcards:
         # insert code to ask question
         # question is question[0] ans asnwer is question [1]
 
         if question_right: # put condition here
-            update_answer(connection, question, True)
+            update_answer(connection, table_name, question, True)
         else:
-            update_answer(connection, question, False)
+            update_answer(connection, table_name, question, False)
     
     connection.commit()
 
 # ask questions from a specific table from the database flashcards_database
-def ask_questions():
+def ask_questions(table_name):
     connection = flashcards_database.connect()
-    flashcards_database.create_tables(connection)
+    flashcards_database.create_tables(connection, table_name)
 
     #get all questions:
-    all_flashcards = get_all_flashcards(connection)
+    all_flashcards = get_all_flashcards(connection, table_name)
 
     # ask all questions:
     for question in all_flashcards:
@@ -31,20 +31,20 @@ def ask_questions():
         # question is question[0] ans asnwer is question [1]
 
         if question_right: # put condition here
-            update_answer(connection, question, True)
+            update_answer(connection, table_name, question, True)
         else:
-            update_answer(connection, question, False)
+            update_answer(connection, table_name, question, False)
     
     connection.commit()
         
     
     
     # ask again flashcards that are wrong until user gets them all right
-    wrong_flashcards = get_wrong_flashcards(connection)
+    wrong_flashcards = get_wrong_flashcards(connection, table_name)
 
     while wrong_flashcards.len > 0:
-        ask_wrong_flashcards(connection, wrong_flashcards)
-        wrong_flashcards = get_wrong_flashcards(connection)
+        ask_wrong_flashcards(connection, table_name, wrong_flashcards)
+        wrong_flashcards = get_wrong_flashcards(connection, table_name)
 
 
     # when we're done
@@ -54,9 +54,9 @@ def ask_questions():
 def menu():
     connection = flashcards_database.connect()
     connection.execute('DROP TABLE IF EXISTS flashcards')
-    flashcards_database.create_tables(connection)
+    flashcards_database.create_tables(connection, 'flashcards')
 
-    print(get_all_flashcards(connection))
+    print(get_all_flashcards(connection, 'flashcards'))
 
 menu()
 
